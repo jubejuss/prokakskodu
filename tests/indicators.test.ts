1cf2c5eada64132e38127151a11f759870cbf13b
Juho Kalberg <juss@vilenkell.eu>
2022-01-05 22:18:14 +0200

    mot logged in tests

--- tests/indicators.test.ts
+++ tests/indicators.test.ts
@@ -5,12 +5,21 @@ import app from '../src/app';
 
 describe('indicators controller', () => {
   describe('GET /indicators', () => {
-    it('responds with code 401 and error message', async () => {
+    it('responds with code 401 and error message because no token', async () => {
       const response = await request(app).get('/indicators');
       expect(response.body).to.be.a('object');
       expect(response.statusCode).to.equal(401);
       expect(response.body).to.have.key('error');
       expect(response.body.error).to.equal('No token provided');
+    });
+    it('responds with code 401 and error message because invalid token', async () => {
+      const response = await request(app)
+        .get('/indicators')
+        .set('Authorization', 'Bearer akshdhajshdjhasjdhjas');
+      expect(response.body).to.be.a('object');
+      expect(response.statusCode).to.equal(401);
+      expect(response.body).to.have.key('error');
+      expect(response.body.error).to.equal('invalid token');
     });
   });
 });
